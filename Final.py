0daf927cc0d1d2935c8163b498ecba9ebdb46197
BombInk <62799527+BombInk@users.noreply.github.com>
2020-05-02 11:57:57 -0400

    Create Final.py

---
+++ Final.py
@@ -0,0 +1,9 @@
+
+import requests
+
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.com/Stonemaier-Games-STM900-Wingspan-Game/' \
+      'dp/B07MC9JN56/ref=sr_1_1?dchild=1&keywords=wingspan&qid=1588433425&rnid=2470954011&sr=8-1'
+
+
